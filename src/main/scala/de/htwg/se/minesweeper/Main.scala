1ee2b38610b1b3b9da3f84ed8124096359839506
Tondoa <jonashammeretyp@gmail.com>
2023-03-24 12:04:33 +0100

    Architecture refactored

---
+++ src/main/scala/de/htwg/se/minesweeper/Main.scala
@@ -0,0 +1,6 @@
+package de.htwg.se.minesweeper
+@main def hello: Unit = 
+  println("Hello worldd!")
+  println(msg)
+
+def msg = "I was compiled by Scala 3. :)"
---
+++ src/main/scala/de/htwg/se/minesweeper/model/Matrix.scala
@@ -0,0 +1,4 @@
+package de.htwg.se.minesweeper.model
+
+final case class Matrix[T](rows: Vector[Vector[T]])
+//tabulate
