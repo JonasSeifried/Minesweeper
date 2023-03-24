19ae4d4325fde017b480d42910a6e8be5d6ad25a
Tondoa <jonashammeretyp@gmail.com>
2023-03-24 12:14:46 +0100

    letzer merge test

--- src/main/scala/de/htwg/se/minesweeper/Main.scala
+++ src/main/scala/de/htwg/se/minesweeper/Main.scala
@@ -1,8 +1,11 @@
 package de.htwg.se.minesweeper
+
 @main def hello: Unit = 
   println("Hello world my project is Minesweeper!")
   println(msg)
   print("Hello")
   print("bitte jetzt")
+  "letzer test"
+  
 
 def msg = "I was compiled by Scala 3. :)"
