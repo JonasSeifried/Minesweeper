d3f7d15b7fa075cd7ef4e0fdd87cc54442468d00
Tondoa <jonashammeretyp@gmail.com>
2023-03-24 12:08:37 +0100

    test merge

--- src/main/scala/de/htwg/se/minesweeper/Main.scala
+++ src/main/scala/de/htwg/se/minesweeper/Main.scala
@@ -2,6 +2,7 @@ package de.htwg.se.minesweeper
 @main def hello: Unit = 
   println("Hello world my project is Minesweeper!")
   println(msg)
-  print("Test da merge")
+  print("Test da merge2")
+  "mehr code"
 
 def msg = "I was compiled by Scala 3. :)"
