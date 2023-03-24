ebf12ba3c4b1d85df409de4e7db2760195b2008a
berrakuzun <uzun.berrak58@gmail.com>
2023-03-24 12:11:39 +0100

    test

--- src/main/scala/de/htwg/se/minesweeper/Main.scala
+++ src/main/scala/de/htwg/se/minesweeper/Main.scala
@@ -2,5 +2,6 @@ package de.htwg.se.minesweeper
 @main def hello: Unit = 
   println("Hello world my project is Minesweeper!")
   println(msg)
+  print("Hello")
 
 def msg = "I was compiled by Scala 3. :)"
