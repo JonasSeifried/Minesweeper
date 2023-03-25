444ba033290515a76195404aa57a38eca3272a3b
Tondoa <69268825+JonasSeifried@users.noreply.github.com>
2023-03-25 14:08:44 +0100

    Tile +TileMatrix

--- src/main/scala/de/htwg/se/minesweeper/Main.scala
+++ src/main/scala/de/htwg/se/minesweeper/Main.scala
@@ -1,8 +1,15 @@
 package de.htwg.se.minesweeper
 
+import de.htwg.se.minesweeper.model._
+
 @main def hello: Unit =
   println("Hello world my project is Minesweeper!")
   println(msg)
 
+  val field = new TileMatrix(15, 15)
+  println(field)
+  val tile = field.getTile(10, 2)
+  println(tile.isBomb)
 
-def msg = "I was compiled by Scala 3. :)"
+
+def msg = "I was compiled by Scala 3. 💣:)"
