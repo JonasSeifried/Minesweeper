4b3ed0001cae5cbb5811f2d7a59cf2cc9b03ac5b
JonasSeifried <69268825+JonasSeifried@users.noreply.github.com>
2023-03-31 12:21:31 +0200

    added test folders and dependency

--- src/main/scala/de/htwg/se/minesweeper/Main.scala
+++ src/main/scala/de/htwg/se/minesweeper/Main.scala
@@ -2,11 +2,11 @@ package de.htwg.se.minesweeper
 
 import de.htwg.se.minesweeper.model._
 
-@main def hello: Unit =
+@main def hello(): Unit =
   println("Hello world my project is Minesweeper!")
   println(msg)
 
-  val field = new TileMatrix(15, 15)
+  val field = TileMatrix(15, 15)
   println(field)
   val tile = field.getTile(10, 2)
   println(tile.isBomb)
---
+++ src/test/scala/de/htwg/se/minesweeper/model/TileMatrixSpec.scala.scala
@@ -0,0 +1,9 @@
+
+package de.htwg.se.minesweeper.model
+
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers._
+
+class TileMatrixSpec extends AnyWordSpec {
+    
+}
