28edc3ff48d4d96fb9a0934c438792bbd2abf5b9
JonasSeifried <69268825+JonasSeifried@users.noreply.github.com>
2023-04-12 11:54:26 +0200

    added TilrMatrix test

--- src/test/scala/de/htwg/se/minesweeper/model/TileMatrixSpec.scala.scala
+++
@@ -1,9 +0,0 @@
-
-package de.htwg.se.minesweeper.model
-
-import org.scalatest.wordspec.AnyWordSpec
-import org.scalatest.matchers.should.Matchers._
-
-class TileMatrixSpec extends AnyWordSpec {
-    
-}
