624117ac659813ddf595741bfc8b263c50615cd4
berrakuzun <uzun.berrak58@gmail.com>
2023-05-30 14:08:32 +0200

    test Controller

--- src/test/scala/de/htwg/se/minesweeper/controller/ControllerSpec.scala
+++ src/test/scala/de/htwg/se/minesweeper/controller/ControllerSpec.scala
@@ -1,16 +1,14 @@
-package de.htwg.se.minesweeper.controller
-
-import de.htwg.se.minesweeper.model.Difficulty.Easy
+import de.htwg.se.minesweeper.controller.{Controller, FlagCommand}
+import de.htwg.se.minesweeper.model.{Difficulty, Field, Tile}
+import de.htwg.se.minesweeper.util.State.{InGameState, PostGameState}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
-import de.htwg.se.minesweeper.model.{Field, Tile}
-import de.htwg.se.minesweeper.util.State.InGameState
 
 class ControllerSpec extends AnyWordSpec with Matchers {
 
   "A Minesweeper Controller" when {
     "new" should {
-      val field = new Field(10, 10, Easy)
+      val field = new Field(10, 10, Difficulty.Easy)
       val controller = Controller(field)
       controller.state = InGameState(controller)
 
@@ -37,13 +35,78 @@ class ControllerSpec extends AnyWordSpec with Matchers {
         }
       }
       "saving and restoring" should {
-        "saving the game" in {
+        "save the game" in {
           controller.saveGame() should be(true)
         }
-        "restore game" in {
+        "restore the game" in {
           val field = controller.field
           controller.restoreGame()
           controller.field should be(field)
+        }
+      }
+      "undo the last step" should {
+        "return true and revert the field" in {
+          val flagCommand = new FlagCommand(3, 5)
+          val initialField = controller.field
+          val flaggedField = flagCommand.doStep(initialField)
+          controller.field = flaggedField
+
+          val result = controller.undo
+
+          result should be(true)
+          controller.field should be(initialField)
+        }
+
+        "return false if there is no previous step" in {
+          val initialField = controller.field
+
+          val result = controller.undo
+
+          result should be(false)
+          controller.field should be(initialField)
+        }
+      }
+      "flag a tile" should {
+        "return true and flag the tile" in {
+          val initialField = controller.field
+          val x = 3
+          val y = 5
+
+          val result = controller.flagTile(x, y)
+
+          result should be(true)
+          controller.field.getTile(x, y).isFlagged should be(true)
+        }
+
+        "return false if the coordinates are out of bounds" in {
+          val initialField = controller.field
+          val x = 100
+          val y = 100
+
+          val result = controller.flagTile(x, y)
+
+          result should be(false)
+          controller.field should be(initialField)
+        }
+
+        "update the game state to PostGameState if game is won" in {
+          val initialField = controller.field
+          val x = 3
+          val y = 5
+
+          // Set the remaining unopened tiles to bombs
+          for {
+            row <- 0 until controller.getRowSize
+            col <- 0 until controller.getColSize
+            if !(row == x && col == y)
+          } {
+            controller.field = controller.field.replaceTile(row, col, Tile(true, 0, false, false))
+          }
+
+          val result = controller.flagTile(x, y)
+
+          result should be(true)
+          controller.state should be(PostGameState(controller))
         }
       }
     }
