f320c399d56c01513944ebc6cec97ba61e3492b8
berrakuzun <uzun.berrak58@gmail.com>
2023-05-30 14:49:23 +0200

    fix bugs

--- src/test/scala/de/htwg/se/minesweeper/model/CoordinateManagerSpec.scala
+++ src/test/scala/de/htwg/se/minesweeper/model/CoordinateManagerSpec.scala
@@ -1,22 +1,20 @@
-package de.htwg.se.minesweeper.model
-
-import de.htwg.se.minesweeper.util.CoordinateManager
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers.*
+import de.htwg.se.minesweeper.util.CoordinateManager
 
 class CoordinateManagerSpec extends AnyWordSpec {
   val coordinateManager = new CoordinateManager
   "A CoordinateManager" should {
-    "decrypt coordinates in for of b12 (a-Z0-99) and flip them" should {
+    "decrypt coordinates in the form of b12 (a-Z0-99) and flip them" should {
       "input.length == 2" in {
-        coordinateManager.decrypt("a1") should be(1, 0)
+        coordinateManager.decrypt("a1") should be(Some(1, 0))
       }
       "input.length == 3" in {
-          coordinateManager.decrypt("a01") should be(1, 0)
+        coordinateManager.decrypt("a01") should be(Some(1, 0))
       }
     }
-    "input to short or too long" in {
-      coordinateManager.decrypt("a123") should be(-1,-1)
+    "input too short or too long" in {
+      coordinateManager.decrypt("a123") should be(None)
     }
   }
 }
