0ea2ec9e1156b8fd7dd7132d4baf99df3f3b016f
berrakuzun <uzun.berrak58@gmail.com>
2023-05-31 19:07:27 +0200

    test OpenCommand

--- src/test/scala/de/htwg/se/minesweeper/controller/OpenCommandSpec.scala
+++ src/test/scala/de/htwg/se/minesweeper/controller/OpenCommandSpec.scala
@@ -1,35 +1,35 @@
 package de.htwg.se.minesweeper.controller
 
 import de.htwg.se.minesweeper.model.{Difficulty, Field, Tile}
-import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest._
 import org.scalatest.matchers.should.Matchers
-import org.scalatest.BeforeAndAfterEach
+import org.scalatest.wordspec.AnyWordSpec
 
-class OpenCommandSpec extends AnyFlatSpec with Matchers with BeforeAndAfterEach {
-  private var field: Field = _
+class OpenCommandSpec extends AnyWordSpec with Matchers {
+  "An OpenCommand" when {
+    "executed on a Field" should {
+      val sizeX = 5
+      val sizeY = 5
+      val field = new Field(sizeX, sizeY, Difficulty.Easy)
+      val command = new OpenCommand(2, 2)
 
-  override protected def beforeEach(): Unit = {
-    field = new Field(5, 5, Difficulty.Easy)
-  }
+      "open the specified tile" in {
+        val newField = command.doStep(field)
+        newField.getTile(2, 2).isHidden shouldBe false
+      }
 
-  "An OpenCommand" should "open a tile at the specified coordinates when executing the doStep method" in {
-    val command = new OpenCommand(2, 2)
-    val updatedField = command.doStep(field)
-    updatedField.getTile(2, 2).isOpened shouldBe true
-  }
+      "close the previously opened tile when undone" in {
+        val openedField = command.doStep(field)
+        val closedField = command.undoStep(openedField)
+        closedField.getTile(2, 2).isHidden shouldBe true
+      }
 
-  it should "close a previously opened tile when executing the undoStep method" in {
-    val command = new OpenCommand(2, 2)
-    field = command.doStep(field)
-    val updatedField = command.undoStep(field)
-    updatedField.getTile(2, 2).isOpened shouldBe false
-  }
-
-  it should "open a tile again at the specified coordinates when executing the redoStep method" in {
-    val command = new OpenCommand(2, 2)
-    field = command.doStep(field)
-    val updatedField = command.undoStep(field)
-    val redoUpdatedField = command.redoStep(updatedField)
-    redoUpdatedField.getTile(2, 2).isOpened shouldBe true
+      "open the previously closed tile when redone" in {
+        val openedField = command.doStep(field)
+        val closedAndOpenedField = command.undoStep(openedField)
+        val reopenedField = command.redoStep(closedAndOpenedField)
+        reopenedField.getTile(2, 2).isHidden shouldBe false
+      }
+    }
   }
 }
