c4fd8ff36e0c8f041c40a61c38b5777b1e416fc8
berrakuzun <uzun.berrak58@gmail.com>
2023-06-02 12:20:11 +0200

    test flagTile

--- src/test/scala/de/htwg/se/minesweeper/controller/ControllerSpec.scala
+++ src/test/scala/de/htwg/se/minesweeper/controller/ControllerSpec.scala
@@ -11,60 +11,93 @@ class ControllerSpec extends AnyWordSpec with Matchers {
   "A Minesweeper Controller" when {
     "new" should {
       val field = new Field(10, 10, Easy)
-      val controller = Controller(field)
-      controller.state = InGameState(controller)
+      var controller: Option[Controller] = None
 
       "have a renewField function" in {
-        val newField = controller.renewField()
-        newField should not be field
-        newField.rowSize should be(field.rowSize)
-        newField.colSize should be(field.colSize)
+        controller = Some(Controller(field))
+        controller.foreach { c =>
+          c.state = InGameState(c)
+          val newField = c.renewField()
+          newField should not be field
+          newField.rowSize should be(field.rowSize)
+          newField.colSize should be(field.colSize)
+        }
       }
-      "printed the Field with the toString-Methode" in {
-        controller.toString should be(field.toString)
-      }
-      "get if Tile is hidden" in {
-        controller.openTile(0,0)
-        controller.getTileIsHidden(0,0) should be (false)
-      }
-      "get if a Tile is a Bomb" in {
-        controller.field = controller.field.replaceTile(0,0, Tile(true, 0, false, false))
-        controller.getTileIsBomb(0,0) should be (true)
-      }
+
+      "printed the Field with the toString-Methode" in
+        controller.foreach(c => c.toString should be(field.toString))
+
+
+      "get if Tile is hidden" in
+        controller.foreach { c =>
+          c.openTile(0,0)
+          c.getTileIsHidden(0,0) should be (false)
+        }
+
+      "get if a Tile is a Bomb" in
+        controller.foreach { c =>
+          c.field = c.field.replaceTile(0,0, Tile(true, 0, false, false))
+          c.getTileIsBomb(0,0) should be (true)
+        }
+
       "get a Tile" should {
         "if out of bounds return null" in {
-          controller.getTile(100,100) should be(null)
+          controller.foreach(c => c.getTile(100,100) should be(null))
         }
       }
+
       "saving and restoring" should {
         "saving the game" in {
-          controller.saveGame() should be(true)
+          controller.foreach(c => c.saveGame() should be(true))
         }
         "restore game" in {
-          val field = controller.field
-          controller.restoreGame()
-          controller.field should be(field)
+          controller.foreach { c =>
+            val field = c.field
+            c.restoreGame()
+            c.field should be(field)
+          }
         }
       }
+
       "set the state to PostGameState when the game is won" in {
         var gameWon = true // Assume game won initially
         import scala.util.control.Breaks._
-        // Open all tiles except for one bomb tile
-        breakable {
-          for (i <- 0 until controller.getRowSize) {
-            for (j <- 0 until controller.getColSize) {
-              if (!controller.getTileIsBomb(i, j)) {
-                controller.openTile(i, j)
-                if (!controller.gameWon) {
-                  gameWon = false // Set the flag to indicate game not won
-                  break() // Exit the inner loop
+        controller.foreach { c =>
+          c.state = InGameState(c)
+          // Open all tiles except for one bomb tile
+          breakable {
+            for (i <- 0 until c.getRowSize) {
+              for (j <- 0 until c.getColSize) {
+                if (!c.getTileIsBomb(i, j)) {
+                  c.openTile(i, j)
+                  if (!c.gameWon) {
+                    gameWon = false // Set the flag to indicate game not won
+                    break() // Exit the inner loop
+                  }
                 }
               }
+              if (!gameWon) break() // Exit the outer loop if game not won
             }
-            if (!gameWon) break() // Exit the outer loop if game not won
           }
         }
-        controller.state.isPostGameState should be(true)
+        controller.foreach(c => c.state.isPostGameState should be(true))
+      }
+
+      "flag a Tile" should {
+        "return false if the given coordinates are out of bounds" in
+          controller.foreach { c =>
+            c.flagTile(-1, 0) should be(false)
+            c.flagTile(0, -1) should be(false)
+            c.flagTile(c.getRowSize, 0) should be(false)
+            c.flagTile(0, c.getColSize) should be(false)
+          }
+
+        "return true and flag the tile at the given coordinates" in
+          controller.foreach { c =>
+            val tile = c.getTile(0, 0)
+            c.flagTile(0, 0) should be(true)
+            val fieldString = c.toString
+          }
       }
     }
   }
