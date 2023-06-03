76ea9d81b343ddd163910fd91fab7b7f8c84576f
JonasSeifried <69268825+JonasSeifried@users.noreply.github.com>
2023-06-03 11:43:56 +0200

    added MenuBar and part of game logic

--- src/main/scala/de/htwg/se/minesweeper/aview/Gui.scala
+++ src/main/scala/de/htwg/se/minesweeper/aview/Gui.scala
@@ -3,24 +3,35 @@ package de.htwg.se.minesweeper.aview
 import de.htwg.se.minesweeper.controller.Controller
 import de.htwg.se.minesweeper.util.Observer
 
-import scala.swing.{BorderPanel, Button, Frame, GridPanel, Label}
-class Gui(controller: Controller) extends Frame with Observer{
+import scala.swing.*
+import scala.swing.event.MouseClicked
+class Gui(controller: Controller) extends Frame with Observer:
   controller.add(this)
   title = "Minesweeper"
-
-  contents = new BorderPanel {
-    add(new Label("Minesweeper"), BorderPanel.Position.North)
-    add(gridCreate(controller.getRowSize, controller.getColSize), BorderPanel.Position.Center)
+  menuBar = new MenuBar {
+    contents += new Menu("File") {
+      contents += new MenuItem(Action("Exit") {
+        sys.exit(0)
+      })
+    }
   }
+
+  contents = updateContent()
 
 
   pack()
   centerOnScreen()
   open()
 
-  override def update(): Unit = ???
+  private def updateContent() =
+    new BorderPanel {
+      add(new Label("Minesweeper"), BorderPanel.Position.North)
+      add(gridCreate(controller.getRowSize, controller.getColSize), BorderPanel.Position.Center)
+    }
 
-  def gridCreate(rowSize: Int, colSize: Int): GridPanel =
+  override def update(): Unit = contents = updateContent();  repaint()
+
+  private def gridCreate(rowSize: Int, colSize: Int): GridPanel =
     new GridPanel(rowSize, colSize) {
       (for (
         x <- 0 until rowSize;
@@ -28,19 +39,29 @@ class Gui(controller: Controller) extends Frame with Observer{
       ) yield (x, y)).foreach(t => contents += btnCreate(t._1, t._2))
     }
 
-  def btnCreate(row: Int, col: Int): Button =
+  private def btnCreate(row: Int, col: Int): Button =
     new Button(tileToString(row, col)) {
+      focusPainted = false
+      borderPainted = false
+      listenTo(mouse.clicks)
+
+      reactions += {
+        case evt @ MouseClicked(src, pt, mod, clicks, props) => {
+          val mouseBtn = evt.peer.getButton
+          if(mouseBtn == 1)
+            controller.openTile(row, col)
+          else if(mouseBtn == 3)
+            controller.flagTile(row, col)
+        }
+      }
 
     }
 
-  def tileToString(row: Int, col: Int): String =
+  private def tileToString(row: Int, col: Int): String =
     if(controller.getTileIsHidden(row, col)) return "⬜"
+    if(controller.getTileIsFlagged(row, col)) return "🚩"
     if(controller.getTileIsBomb(row, col)) return "💣"
     controller.getTile(row, col).toString
-}
-
-
-
 
 
 
