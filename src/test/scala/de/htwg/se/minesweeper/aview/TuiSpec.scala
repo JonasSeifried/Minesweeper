0f8347ef51f861c548301aa62adba93febe527c6
JonasSeifried <69268825+JonasSeifried@users.noreply.github.com>
2023-06-09 12:59:41 +0200

    Tuispec bugfix

--- src/test/scala/de/htwg/se/minesweeper/aview/TuiSpec.scala
+++ src/test/scala/de/htwg/se/minesweeper/aview/TuiSpec.scala
@@ -15,8 +15,6 @@ class TuiSpec extends AnyWordSpec {
   val field: Field = new Field(3, 3, Easy).openTile(0, 0)
   val controller: Controller = Controller(field)
   val tui = new Tui(controller)
-
-  "A TUI" when {
     "processing input" should {
       "renew the field when input is 'n'" in {
         val initialField = controller.field.copy()
@@ -27,10 +25,10 @@ class TuiSpec extends AnyWordSpec {
         tui.processInput("") should be(false)
       }
       "return true when input starts with 'o' and valid input" in {
-        tui.processInput("o a2") should be(false)
+        tui.processInput("oa2") should be(true)
       }
       "return true when input starts with 'f' and valid input" in {
-        tui.processInput("f a1") should be(false)
+        tui.processInput("fa1") should be(true)
       }
       "return false when input starts with 'o' or 'f' and bad input" should {
         "return false when input length < 4" in {
@@ -103,82 +101,80 @@ class TuiSpec extends AnyWordSpec {
       "return false when input starts with anything else" in {
         tui.processInput("csad") should be(false)
       }
-      "return true when input is 'r' and redo is successful" in {
-        tui.processInput("r") should be(false)
-      }
+    }
+    "undo and redo" should {
+      tui.processInput("ob1")
       "return true when input is 'u' and undo is successful" in {
-        tui.processInput("u") should be(false)
+        tui.processInput("u") should be(true)
+      }
+      "return true when input is 'r' and redo is successful" in {
+        tui.processInput("u")
+        tui.processInput("r") should be(true)
       }
     }
-  }
 
-  "run() method" should {
-    "return true if inputLoop() returns true" in {
-      val outputStream = new ByteArrayOutputStream()
-      Console.withOut(outputStream) {
-        val tui = new Tui(controller) {
-          override def inputLoop(): Boolean = true
+    "run() method" should {
+      "return true if inputLoop() returns true" in {
+        val outputStream = new ByteArrayOutputStream()
+        Console.withOut(outputStream) {
+          val tui = new Tui(controller) {
+            override def inputLoop(): Boolean = true
+          }
+          val result = tui.run()
+          result should be(true)
         }
-        val result = tui.run()
-        result should be(true)
       }
-    }
 
-    "return false if inputLoop() returns false" in {
-      val outputStream = new ByteArrayOutputStream()
-      Console.withOut(outputStream) {
-        val tui = new Tui(controller) {
-          override def inputLoop(): Boolean = false
+      "return false if inputLoop() returns false" in {
+        val outputStream = new ByteArrayOutputStream()
+        Console.withOut(outputStream) {
+          val tui = new Tui(controller) {
+            override def inputLoop(): Boolean = false
+          }
+          val result = tui.run()
+          result should be(false)
         }
-        val result = tui.run()
-        result should be(false)
-      }
-    }
-  }
-
-  "The update() method" should {
-    "print correct messages when in post-game state" in {
-      val controllerGameWon = new Controller(field) {
-        override def isPostGameState: Boolean = true
-        override def gameWon: Boolean = true
-        override def gameOver: Boolean = false
-      }
-
-      val tuiGameWon = new Tui(controllerGameWon)
-
-      val outputStreamGameWon = new ByteArrayOutputStream()
-      Console.withOut(outputStreamGameWon) {
-        tuiGameWon.update(Event.GameOver)
-        val consoleOutputGameWon = outputStreamGameWon.toString
-        consoleOutputGameWon should include("Spiel gewonnen!")
-      }
-
-      val controllerGameOver = new Controller(field) {
-        override def isPostGameState: Boolean = true
-        override def gameWon: Boolean = false
-        override def gameOver: Boolean = true
-      }
-
-      val tuiGameOver = new Tui(controllerGameOver)
-
-      val outputStreamGameOver = new ByteArrayOutputStream()
-      Console.withOut(outputStreamGameOver) {
-        tuiGameOver.update(Event.GameOver)
-
-        val consoleOutputGameOver = outputStreamGameOver.toString
-        consoleOutputGameOver should include("Spiel verloren!")
-      }
-    }
-  }
-  "inputLoop() method" should {
-    "return false when input is empty" in {
-      val inputStream = new ByteArrayInputStream("\n".getBytes)
-      Console.withIn(inputStream) {
-        val result = tui.inputLoop()
-        result should be(false)
       }
     }
 
+    "The update() method" should {
+      "print correct messages when in post-game state" in {
+        val controllerGameWon = new Controller(field) {
+          override def isPostGameState: Boolean = true
+
+          override def gameWon: Boolean = true
+
+          override def gameOver: Boolean = false
+        }
+
+        val tuiGameWon = new Tui(controllerGameWon)
+
+        val outputStreamGameWon = new ByteArrayOutputStream()
+        Console.withOut(outputStreamGameWon) {
+          tuiGameWon.update(Event.GameOver)
+          val consoleOutputGameWon = outputStreamGameWon.toString
+          consoleOutputGameWon should include("Spiel gewonnen!")
+        }
+
+        val controllerGameOver = new Controller(field) {
+          override def isPostGameState: Boolean = true
+
+          override def gameWon: Boolean = false
+
+          override def gameOver: Boolean = true
+        }
+
+        val tuiGameOver = new Tui(controllerGameOver)
+
+        val outputStreamGameOver = new ByteArrayOutputStream()
+        Console.withOut(outputStreamGameOver) {
+          tuiGameOver.update(Event.GameOver)
+
+          val consoleOutputGameOver = outputStreamGameOver.toString
+          consoleOutputGameOver should include("Spiel verloren!")
+        }
+      }
+    }
     "inputLoop() method" should {
       "return false when input is empty" in {
         val inputStream = new ByteArrayInputStream("\n".getBytes)
@@ -187,7 +183,6 @@ class TuiSpec extends AnyWordSpec {
           result should be(false)
         }
       }
-
       "return false when input is 'q'" in {
         val inputStream = new ByteArrayInputStream("q\n".getBytes)
         Console.withIn(inputStream) {
@@ -213,6 +208,4 @@ class TuiSpec extends AnyWordSpec {
         }
       }
     }
-  }
 }
-
