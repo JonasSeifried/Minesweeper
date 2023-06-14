89d0d00551e88b88fc6408e7de3fdd1119156975
JonasSeifried <69268825+JonasSeifried@users.noreply.github.com>
2023-06-15 00:13:30 +0200

    Updated Commands: Field -> FieldInterface

--- src/main/scala/de/htwg/se/minesweeper/controller/FlagCommand.scala
+++ src/main/scala/de/htwg/se/minesweeper/controller/FlagCommand.scala
@@ -1,12 +1,12 @@
 package de.htwg.se.minesweeper
 package controller
 
-import model.Field
+import de.htwg.se.minesweeper.model.fieldComponent.FieldInterface
 import util.Command
 import util.UndoManager
 
 
-class FlagCommand(x: Int, y: Int) extends Command[Field]:
-  override def doStep(field: Field): Field = field.flagTile(x, y)
-  override def undoStep(field: Field): Field = field.flagTile(x, y)
-  override def redoStep(field: Field): Field = field.flagTile(x, y)
+class FlagCommand(x: Int, y: Int) extends Command[FieldInterface]:
+  override def doStep(field: FieldInterface): FieldInterface = field.flagTile(x, y)
+  override def undoStep(field: FieldInterface): FieldInterface = field.flagTile(x, y)
+  override def redoStep(field: FieldInterface): FieldInterface = field.flagTile(x, y)
--- src/main/scala/de/htwg/se/minesweeper/controller/OpenCommand.scala
+++ src/main/scala/de/htwg/se/minesweeper/controller/OpenCommand.scala
@@ -1,11 +1,11 @@
 package de.htwg.se.minesweeper
 package controller
 
-import model.Field
+import de.htwg.se.minesweeper.model.fieldComponent.FieldInterface
 import util.Command
 import util.UndoManager
 
-class OpenCommand(x: Int, y: Int) extends Command[Field]:
-  override def doStep(field: Field): Field = field.openTile(x, y)
-  override def undoStep(field: Field): Field = field.closeTile(x, y)
-  override def redoStep(field: Field): Field = field.openTile(x, y)
+class OpenCommand(x: Int, y: Int) extends Command[FieldInterface]:
+  override def doStep(field: FieldInterface): FieldInterface = field.openTile(x, y)
+  override def undoStep(field: FieldInterface): FieldInterface = field.closeTile(x, y)
+  override def redoStep(field: FieldInterface): FieldInterface = field.openTile(x, y)
