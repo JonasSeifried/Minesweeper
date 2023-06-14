7bb9887eed32a21abda072a65003c8b7f4029478
JonasSeifried <69268825+JonasSeifried@users.noreply.github.com>
2023-06-15 00:15:14 +0200

    Updated SaveManager: Field -> FieldInterface

--- src/main/scala/de/htwg/se/minesweeper/model/SaveManager.scala
+++ src/main/scala/de/htwg/se/minesweeper/model/SaveManager.scala
@@ -1,10 +1,12 @@
 package de.htwg.se.minesweeper.model
 
+import de.htwg.se.minesweeper.model.fieldComponent.FieldInterface
+
 import java.io.{FileInputStream, FileOutputStream, ObjectInputStream, ObjectOutputStream}
-import scala.util.{Try, Success, Failure}
+import scala.util.{Failure, Success, Try}
 
 object SaveManager:
-  def saveGame(field: Field): Boolean =
+  def saveGame(field: FieldInterface): Boolean =
     val fileStream = new FileOutputStream("gameData")
     val outputStream = new ObjectOutputStream(fileStream)
     val result =Try(outputStream.writeObject(field))
@@ -12,10 +14,10 @@ object SaveManager:
     outputStream.close()
     result.isSuccess
 
-  def restoreGame(): Field =
+  def restoreGame(): FieldInterface =
     val fileStream = new FileInputStream("gameData")
     val objStream = new ObjectInputStream(fileStream)
-    val result =Try(objStream.readObject().asInstanceOf[Field])
+    val result =Try(objStream.readObject().asInstanceOf[FieldInterface])
     fileStream.close()
     objStream.close()
     result.getOrElse(null)
