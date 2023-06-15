c96e794a8620bf31c7d39461010bec8ba14ee732
JonasSeifried <69268825+JonasSeifried@users.noreply.github.com>
2023-06-15 21:02:44 +0200

    Added Injector to Minesweeper

--- src/main/scala/de/htwg/se/minesweeper/Minesweeper.scala
+++ src/main/scala/de/htwg/se/minesweeper/Minesweeper.scala
@@ -1,18 +1,19 @@
 package de.htwg.se.minesweeper
 
-import aview.{Tui, Gui}
-import de.htwg.se.minesweeper.controller.controllerComponent.controllerBaseImpl.Controller
+import aview.{Gui, Tui}
+import com.google.inject.Guice
 import model.SaveManager
 import de.htwg.se.minesweeper.model.Difficulty.{Easy, Hard}
-import de.htwg.se.minesweeper.model.fieldComponent.fieldBaseImpl.Field
+import de.htwg.se.minesweeper.controller.controllerComponent.ControllerInterface
 
 import scala.io.StdIn.readLine
 
 object Minesweeper:
+  private val injector = Guice.createInjector(new MinesweeperModule)
+  val controller: ControllerInterface = injector.getInstance(classOf[ControllerInterface])
 
   @main def main(): Unit =
     println("Hello world my project is Minesweeper!")
-    val controller = Controller(new Field(5, 5, Hard).renewField)
     val tui = Tui(controller)
     val gui = Gui(controller)
     tui.run()
