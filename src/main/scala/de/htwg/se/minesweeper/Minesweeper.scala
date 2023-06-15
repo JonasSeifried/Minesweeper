97a06f50fdbe331f85936990ca94451ecf067c7d
JonasSeifried <69268825+JonasSeifried@users.noreply.github.com>
2023-06-15 19:13:18 +0200

    Renamed main and created object Minesweeper

--- src/main/scala/de/htwg/se/minesweeper/Minesweeper.scala
+++ src/main/scala/de/htwg/se/minesweeper/Minesweeper.scala
@@ -8,9 +8,11 @@ import de.htwg.se.minesweeper.model.fieldComponent.fieldBaseImpl.Field
 
 import scala.io.StdIn.readLine
 
-@main def main(): Unit =
-  println("Hello world my project is Minesweeper!")
-  val controller = Controller(new Field(5, 5, Hard).renewField)
-  val tui = Tui(controller)
-  val gui = Gui(controller)
-  tui.run()
+object Minesweeper:
+
+  @main def main(): Unit =
+    println("Hello world my project is Minesweeper!")
+    val controller = Controller(new Field(5, 5, Hard).renewField)
+    val tui = Tui(controller)
+    val gui = Gui(controller)
+    tui.run()
