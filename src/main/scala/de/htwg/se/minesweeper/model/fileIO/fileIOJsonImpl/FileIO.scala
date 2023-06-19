e96529d07d577e886284cc7bd3233072a2dfd3cb
JonasSeifried <69268825+JonasSeifried@users.noreply.github.com>
2023-06-19 11:34:39 +0200

    json FileIo start

--- src/main/scala/de/htwg/se/minesweeper/model/fileIO/fileIOJsonImpl/FileIO.scala
+++ src/main/scala/de/htwg/se/minesweeper/model/fileIO/fileIOJsonImpl/FileIO.scala
@@ -1,5 +1,14 @@
 package de.htwg.se.minesweeper.model.fileIO.fileIOJsonImpl
 
-class FileIO {
+import de.htwg.se.minesweeper.model.fieldComponent.FieldInterface
+import de.htwg.se.minesweeper.model.fileIO.FileIOInterface
 
-}
+class FileIO extends FileIOInterface:
+
+  override def save(field: FieldInterface): Boolean =
+    import java.io._
+    val pw = new PrintWriter(new File("field.json"))
+
+    true
+
+  override def load: FieldInterface = ???
