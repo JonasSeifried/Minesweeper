c9392f092b7be81c08c76057aaf431d0e120fa5f
JonasSeifried <69268825+JonasSeifried@users.noreply.github.com>
2023-06-21 10:50:42 +0200

    refactored MinesweeperModule

--- src/main/scala/de/htwg/se/minesweeper/MinesweeperModule.scala
+++ src/main/scala/de/htwg/se/minesweeper/MinesweeperModule.scala
@@ -8,11 +8,20 @@ import de.htwg.se.minesweeper.controller.controllerComponent.controllerBaseImpl.
 import de.htwg.se.minesweeper.model.Difficulty.{Difficulty, Medium}
 import de.htwg.se.minesweeper.model.fieldComponent.FieldInterface
 import de.htwg.se.minesweeper.model.fieldComponent.fieldAdvancedImpl.Field
+import de.htwg.se.minesweeper.model.fileIO.FileIOInterface
+import de.htwg.se.minesweeper.model.fileIO.fileIOXmlImpl.FileIO as FileIOXml
 
 class MinesweeperModule extends AbstractModule with ScalaModule:
-  private val defaultSize: Int = 5
+  private val small = 5
+  private val normal = 10
+  private val big = 15
 
   override def configure(): Unit =
-    bindConstant().annotatedWith(Names.named("DefaultSize")).to(defaultSize)
+    bindConstant().annotatedWith(Names.named("normal")).to(normal)
+    bind[FieldInterface].annotatedWithName("small").toInstance(new Field(small))
+    bind[FieldInterface].annotatedWithName("normal").toInstance(new Field(normal))
+    bind[FieldInterface].annotatedWithName("big").toInstance(new Field(big))
+
     bind[FieldInterface].to[Field]
     bind[ControllerInterface].to[Controller]
+    bind[FileIOInterface].to[FileIOXml]
